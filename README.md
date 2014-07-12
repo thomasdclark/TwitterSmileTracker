9b88aefbe68cfcef29adb4071180a16784f53ee8
Thomas Clark <clark.2009@osu.edu>
2014-07-12 18:33:21 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,11 @@
+#TwitterEmotionTracker
+
+##Current Functionality
+Receives a stream of tweets containing smilely faces and frowny faces and outputs them to the console.
+
+##Intended Functionality
+Recieves a stream of tweets containing key emotion words that can be interpreted as a spectrum.  Compares the tweet frequency of all of the words and determines the overall feeling of Twitter at that moment in time.
+
+##Third-Party Libraries
+Need to be downloaded from respective sites prior to use:
+* Twitter4J (http://twitter4j.org)
---
+++ src/TwitterEmotionTrackerMain.java
@@ -0,0 +1,95 @@
+import java.util.Scanner;
+
+import twitter4j.FilterQuery;
+import twitter4j.StallWarning;
+import twitter4j.Status;
+import twitter4j.StatusDeletionNotice;
+import twitter4j.StatusListener;
+import twitter4j.TwitterException;
+import twitter4j.TwitterStream;
+import twitter4j.TwitterStreamFactory;
+import twitter4j.conf.ConfigurationBuilder;
+
+/**
+ * Simple GUI application displaying the number of times smiles and frowns have
+ * been tweeted since a specific point in time.
+ * 
+ * @author Thomas Clark
+ */
+public final class TwitterEmotionTrackerMain {
+
+    /**
+     * Private constructor so this utility class cannot be instantiated.
+     */
+    private TwitterEmotionTrackerMain() {
+    }
+
+    /**
+     * Main program
+     * 
+     * @param args
+     *            command-line arguments; not used
+     */
+    public static void main(String[] args) throws TwitterException {
+
+        //OAuth configuration
+        ConfigurationBuilder cb = new ConfigurationBuilder();
+        cb.setDebugEnabled(true);
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Consumer Key:  ");
+        String consumerKey = scan.next();
+        System.out.print("Consumer Secret:  ");
+        String consumerSecret = scan.next();
+        System.out.print("Access Token:  ");
+        String accessToken = scan.next();
+        System.out.print("Access Token Secret:  ");
+        String accessSecret = scan.next();
+        cb.setOAuthConsumerKey(consumerKey);
+        cb.setOAuthConsumerSecret(consumerSecret);
+        cb.setOAuthAccessToken(accessToken);
+        cb.setOAuthAccessTokenSecret(accessSecret);
+
+        //Create stream of tweets
+        TwitterStream twitterStream = new TwitterStreamFactory(cb.build())
+                .getInstance();
+
+        //Determine what to do on each event
+        StatusListener listener = new StatusListener() {
+            @Override
+            public void onStatus(Status status) {
+                System.out.println(status.getUser().getName() + " : "
+                        + status.getText());
+            }
+
+            @Override
+            public void onDeletionNotice(
+                    StatusDeletionNotice statusDeletionNotice) {
+            }
+
+            @Override
+            public void onTrackLimitationNotice(int numberOfLimitedStatuses) {
+            }
+
+            @Override
+            public void onException(Exception ex) {
+                ex.printStackTrace();
+            }
+
+            @Override
+            public void onScrubGeo(long arg0, long arg1) {
+            }
+
+            @Override
+            public void onStallWarning(StallWarning arg0) {
+            }
+        };
+
+        //Tell stream what tweets to filter for
+        FilterQuery fq = new FilterQuery();
+        String keywords[] = { ":)", ":(", "(:", "):" };
+        fq.track(keywords);
+
+        twitterStream.addListener(listener);
+        twitterStream.filter(fq);
+    }
+}
